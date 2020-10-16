b796b30b3df1a7b2ff9d163faad99d18e17b6a72
Jorden Garcia <Jay@Tayllers-MacBook-Pro.local>
2020-10-16 08:06:57 -0600

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './ShoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './ShoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './RecipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './RecipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './RecipeBook/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './RecipeBook/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
